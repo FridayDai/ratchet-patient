d9b7a8cbaead8fc63e88a5de6c631ecf65a6012d
colin <colin.chen@xplusz.com>
2015-02-24 11:59:31 +0800

    RATTWO-351 #comment update login cookie-session to 2.0.16

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -116,12 +116,14 @@ log4j.main = {
 			'net.sf.ehcache.hibernate'
 }
 
+grails.resources.resourceLocatorEnabled = false
 grails.plugin.cookiesession.enabled = true
-grails.plugin.cookiesession.id = "ratchet-session"
-grails.plugin.cookiesession.timeout = 15
-grails.plugin.cookiesession.hmac.id = "grails-session-hmac"
-grails.plugin.cookiesession.hmac.algorithm = "HmacSHA1"
-grails.plugin.cookiesession.hmac.secret = "ratchetByXplusz".bytes.encodeBase64(false).toString()
+grails.plugin.cookiesession.cookiename = "ratchet-session"
+grails.plugin.cookiesession.sessiontimeout = 900
+
+//grails.plugin.cookiesession.hmac.id = "grails-session-hmac"
+grails.plugin.cookiesession.cryptoalgorithm = "HmacSHA1"
+grails.plugin.cookiesession.secret = "ratchetByXplusz".bytes.encodeBase64(false).toString()
 
 // locations to search for config files that get merged into the main config;
 // config files can be ConfigSlurper scripts, Java properties files, or classes
