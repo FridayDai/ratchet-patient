76a537fa769aff5b9aed1801abab2dddaddff11b
Albert Zhang <albert.zhang@xplusz.com>
2015-03-04 19:17:39 +0800

    RATTWO-426 Patient agreement

---
+++ grails-app/controllers/com/xplusz/ratchet/AgreementController.groovy
@@ -0,0 +1,12 @@
+package com.xplusz.ratchet
+
+class AgreementController {
+
+	def termOfService() {
+		render view: '/termOfService'
+	}
+
+	def privacyPolicy() {
+		render view: '/privacyPolicy'
+	}
+}
