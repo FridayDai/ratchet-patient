88c29930b2a259e844720fb530e5070cc45cc9d9
John Xu <john.xu@xplusz.com>
2015-03-16 19:52:43 +0800

    RATTWO-500 #comment Handle client filter exception

--- grails-app/conf/com.xplusz.ratchet/ClientFilters.groovy
+++ grails-app/conf/com.xplusz.ratchet/ClientFilters.groovy
@@ -1,5 +1,10 @@
 package com.xplusz.ratchet
 
+import com.mashape.unirest.http.exceptions.UnirestException
+import com.xplusz.ratchet.exceptions.ApiAccessException
+import com.xplusz.ratchet.exceptions.ApiReturnException
+import org.codehaus.groovy.grails.web.converters.exceptions.ConverterException
+
 class ClientFilters {
 
     def clientService
@@ -13,12 +18,19 @@ class ClientFilters {
 
                     def clientName = hostname.replaceAll(/\.(qa|stable)?\.ratchethealth\.com$/, '')
 
-                    def result = clientService.getClient(request, response, clientName)
+                    try {
+                        def result = clientService.getClient(request, response, clientName)
+                        if (!(result instanceof Integer)) {
+                            session.client = result
+                        }
+                    } catch (ConverterException | MissingPropertyException e) {
+                        log.error("Client Not Found, token: ${session.token}")
 
-                    if (!(result instanceof Integer)) {
-                        session.client = result
-                    } else {
-                        render view: '/error/404', status: 404
+                        if (request.isXhr()) {
+                            render status: 404, text: e.message
+                        } else {
+                            render view: '/error/404', status: 404
+                        }
                         return false
                     }
                 }
