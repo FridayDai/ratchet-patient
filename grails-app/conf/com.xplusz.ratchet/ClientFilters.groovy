d6d7cca7ef48719e0f6e47e03deb32a14697aeb3
Albert Zhang <albert.zhang@xplusz.com>
2015-03-30 14:10:31 +0800

    RATTWO-696 Better logging in ClientFilters

--- grails-app/conf/com.xplusz.ratchet/ClientFilters.groovy
+++ grails-app/conf/com.xplusz.ratchet/ClientFilters.groovy
@@ -2,7 +2,7 @@ package com.xplusz.ratchet
 
 class ClientFilters {
 
-    def clientService
+	def clientService
 
     def filters = {
         all(controller: 'email|task|agreement') {
@@ -13,27 +13,31 @@ class ClientFilters {
 
                     def clientName = hostname.replaceAll(/\.(x|qa|stable)?\.ratchethealth\.com$/, '')
 
-                    try {
-                        def result = clientService.getClient(request, response, clientName)
-                        if (result) {
-							log.info("Client found, Server Name: ${request.getServerName()}")
+					try {
+						def result = clientService.getClient(request, response, clientName)
+						if (result) {
+							log.info("Client found, Server Name: ${request.getServerName()}, Client Domain: ${clientName}")
 
-                            session.client = result
-                        }
-                    } catch (Exception e) {
-                        log.error("Client not found, Server Name: ${request.getServerName()}")
+							session.client = result
+						}
+					} catch (Exception e) {
+						log.error("Client not found, " +
+								"Server Name: ${request.getServerName()}, " +
+								"Client Domain: ${clientName} \n" +
+								"Exception trace: \n${e.stackTrace}"
+						)
 
-                        forward(controller: 'error', action: 'notFound')
-                        return false
-                    }
-                }
-            }
-            after = { Map model ->
+						forward(controller: 'error', action: 'notFound')
+						return false
+					}
+				}
+			}
+			after = { Map model ->
 
-            }
-            afterView = { Exception e ->
+			}
+			afterView = { Exception e ->
 
-            }
-        }
-    }
+			}
+		}
+	}
 }
