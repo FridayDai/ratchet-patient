f2689a57fc9eb7d8271bad6a823b7f896ddb21de
Shawn MacIntyre <shawn@xplusz.com>
2015-03-30 13:48:55 -0700

    RATTWO-699 #comment Fixing parsing order and Adding logging

--- grails-app/services/ratchet/v2/user/desktop/ClientService.groovy
+++ grails-app/services/ratchet/v2/user/desktop/ClientService.groovy
@@ -5,6 +5,7 @@ import com.mashape.unirest.http.exceptions.UnirestException
 import com.xplusz.ratchet.exceptions.ApiAccessException
 import com.xplusz.ratchet.exceptions.ApiReturnException
 import grails.converters.JSON
+import org.codehaus.groovy.grails.web.converters.exceptions.ConverterException
 
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -21,15 +22,18 @@ class ClientService {
 
         try {
             def resp = Unirest.get(clientUrl).asString()
-            def result = JSON.parse(resp.body)
             if (resp.status == 200) {
                 log.info("Get client success, token: ${request.session.token}")
-                return result
+                return JSON.parse(resp.body)
             } else {
                 def message = result?.error?.errorMessage
                 throw new ApiReturnException(message, resp.status)
             }
         } catch (UnirestException e) {
+            throw new ApiAccessException(e.message)
+        } catch (ConverterException e) {
+            log.error "Failed to convert JSON: ${resp}\n" +
+                      "Cause: ${e.getCause()}"
             throw new ApiAccessException(e.message)
         }
     }
