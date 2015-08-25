8f0707701393d77661bbd032a38665587fb503e9
John Xu <john.xu@xplusz.com>
2015-08-25 15:39:23 +0800

    RH #comment Run js lint

--- grails-app/assets/javascripts/bundles/codeValidationBundle.js
+++ grails-app/assets/javascripts/bundles/codeValidationBundle.js
@@ -3,18 +3,6 @@
 (function (undefined) {
     'use strict';
 
-    function createErrorElement(message) {
-        var error = document.createElement('span');
-        error.className = 'rc-error-label';
-        error.innerText = message;
-
-        return error;
-    }
-
-    function isMobileOrTablet() {
-        return window.innerWidth < 992;
-    }
-
     function setConfirmation() {
         var coverEl = document.getElementById('mobile-alert-cover');
         var containerEl = document.getElementById('mobile-alert-container');
