9d41f640278c517cd26a424c005d0fc4e574658f
Colin Chen <colin.chen@xplusz.com>
2015-11-30 17:30:20 +0800

    RH-1905 #comment adjust all font-family to Verdana and notification title to upperCase.

--- grails-app/assets/javascripts/components/task/painChart/PainPercentPanel.js
+++ grails-app/assets/javascripts/components/task/painChart/PainPercentPanel.js
@@ -71,7 +71,7 @@ function PainPercentPanel() {
         var me = this;
 
         Notifications.confirm({
-            title: 'Are you sure?',
+            title: 'ARE YOU SURE?',
             message: 'Enabling this checkbox will clear the answers above'
         }, {
             buttons: [
